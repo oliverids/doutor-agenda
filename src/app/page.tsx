c74ebe0177f8c979e7082bfa3e8c4dd12703c6a4
Isabela Oliveira <oliver.isabela2303@gmail.com>
2025-06-06 18:19:56 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="flex items-center justify-around bg-red-400 text-white">
         hello world
       </h1>
+      <Button>Click me</Button>
     </div>
   );
-}
+}  
